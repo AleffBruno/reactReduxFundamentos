a5f33a96acc07903ca502f815588b51253cd400e
Aleff <aleffbruno@gmail.com>
2019-03-12 14:57:05 -0300

    initialize myMoneyApp

---
+++ myMoney/src/main/app.jsx
@@ -0,0 +1,10 @@
+import '../common/tempalte/dependencies'
+import React from 'react';
+
+import Header from '../common/tempalte/header'
+
+export default props => (
+    <div className="wrapper">
+        <Header />
+    </div>
+)
