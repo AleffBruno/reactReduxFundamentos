81e25a0e72f7cd7066a7cd456856e1d4c9e148cd
Aleff <aleffbruno@gmail.com>
2019-03-13 16:30:29 -0300

    add redux to project

--- myMoney/src/dashboard/dashboard.jsx
+++ myMoney/src/dashboard/dashboard.jsx
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
+
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão1.0'/>
                 <Content>
-                    Dashboard
-                </Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de creditos'/>
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de debitos'/>
+                        <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} text='Valor consolidado'/>
+                    </Row>
+                    </Content>
             </div>
         )
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({
+    summary: state.dashboard.summary
+})
+
+export default connect(mapStateToProps)(Dashboard)
+//export default Dashboard
