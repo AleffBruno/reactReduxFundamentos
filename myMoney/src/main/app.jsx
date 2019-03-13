52ff764cc43e4484656f2d55b3d184f05c741d89
Aleff Bruno <aleffbruno@gmail.com>
2019-03-12 22:53:23 -0300

    add sidebar myMoney

--- myMoney/src/main/app.jsx
+++ myMoney/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../common/tempalte/dependencies'
 import React from 'react';
 
 import Header from '../common/tempalte/header'
+import SideBar from '../common/tempalte/sidebar'
 
 export default props => (
     <div className="wrapper">
         <Header />
+        <SideBar />
     </div>
 )
