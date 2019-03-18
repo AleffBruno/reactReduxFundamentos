5d72962a45031009bb2ecba0595bf87294121946
Aleff <aleffbruno@gmail.com>
2019-03-18 08:01:10 -0300

    add tabContent

--- myMoney/src/billingCycle/billingCycle.jsx
+++ myMoney/src/billingCycle/billingCycle.jsx
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
-import Tabs from '../common/tab/tabs'
-import TabsHeader from '../common/tab/tabsHeader'
-import TabsContent from '../common/tab/tabsContent'
-import TabHeader from '../common/tab/tabHeader'
+import Tabs from '../common/tab/tabs';
+import TabsHeader from '../common/tab/tabsHeader';
+import TabsContent from '../common/tab/tabsContent';
+import TabHeader from '../common/tab/tabHeader';
+import TabContent from '../common/tab/tabContent';
 
 class BillingCycles extends Component {
     render() {
@@ -21,7 +22,10 @@ class BillingCycles extends Component {
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
