4874078bf5a9db92a81705f587c9f8c7b5c634cb
= <=>
2019-03-24 02:50:24 +0000

    add function init to clear the form

--- myMoney/src/billingCycle/billingCyclesActions.js
+++ myMoney/src/billingCycle/billingCyclesActions.js
@@ -4,6 +4,7 @@ import { reset as resetForm, initialize } from 'redux-form';
 import { selectTab, showTabs } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES= {}
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -19,14 +20,7 @@ export function create(values) {
         axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação realizada com sucesso')
-            dispatch([
-                // o parametro do resetForm é o nome dado ao formulario
-                resetForm('billingCycleForm'),
-                getList(),
-                selectTab('tabList'),
-                //showTabs('tablist','tabCreate') CUIDADO COM CAMELCASE !!!
-                showTabs('tabList','tabCreate')
-            ])
+            dispatch(init())
         }).catch(e => {
             e.response.data.errors.forEach(error => toastr.error('Erro',error));
         })
@@ -39,5 +33,14 @@ export function showUpdate(billingCycle) {
         showTabs(`tabUpdate`),
         selectTab(`tabUpdate`),
         initialize(`billingCycleForm`,billingCycle)
+    ]
+}
+
+export function init() {
+    return [
+        showTabs('tabList','tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm',INITIAL_VALUES)
     ]
 }
