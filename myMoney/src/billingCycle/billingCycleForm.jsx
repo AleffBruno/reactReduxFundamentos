51577295689b50351fc5a3cf35da68ab44cb848c
= <=>
2019-03-25 21:02:23 +0000

    component input para creditList

--- myMoney/src/billingCycle/billingCycleForm.jsx
+++ myMoney/src/billingCycle/billingCycleForm.jsx
@@ -20,7 +20,7 @@ class BillingCycleForm extends Component {
                         label='Mes' cols='12 4' placeholder='informe o mes'/>
                     <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='ano' cols='12 4' placeholder='informe o ano'/>
-                    <CreditList cols='12 6' />
+                    <CreditList cols='12 6' readOnly={readOnly}/>
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
